67796ecc1adc288753a49bf34e0afec6ffb11918
Loic <loic.le.bergeron@outlook.com>
2023-09-21 11:32:32 -0400

    Ajoutez des fichiers projet.

---
+++ test github 2.0/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//1) demander le nom de l'utilisateur et l'accueillir
+//ex: bienvenue ( ) à l'application monOUTIL
+
+
+
+Console.WriteLine("Quel est votre nom?:");
+
+String LINE = Console.ReadLine();
+
+Console.WriteLine($"Bienvenue" + LINE + "à l'application");
+
+//demander température en celsius
+Console.WriteLine($"Entrez une température en degré celsius!");
+double températureenCelsius= Convert.ToDouble(Console.ReadLine());
+double températureenFareneit = températureenCelsius * 9 / 5 + 32;
+
+
+Console.WriteLine($"Votre température en fareneit est");
+
+
